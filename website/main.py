c398ce1c950a8a6e6bff83ed8ca96648a8c6b093
XY <oc2245@columbia.edu>
2019-04-24 14:47:55 -0400

    added basic query checking

--- website/main.py
+++ website/main.py
@@ -61,15 +61,88 @@ def logout():
 def GetData():
     columns = request.form.get("columns")
     table = request.form.get("table")
-    print(table)
     condition = request.form.get("condition")
     username = session['username']
-    if condition:
-        condition = " where " + condition
-    print("???")
-    query = "select "+ columns + " from " + table + condition
-    print(username)
-    print(query)
+
+    if table == "accounts":
+        colnames = ["account_id", "district_id", "frequency", "date", "bank_id"]
+        if columns in colnames:
+            if condition:
+                condition = " where " + condition
+            print("???")
+            query = "select "+ columns + " from " + table + condition
+        else:
+            print("no such column")
+    elif table == "card":
+        colnames = ["card_id", "disp_id", "type", "issued", "bank_id"]
+        if columns in colnames:
+            if condition:
+                condition = " where " + condition
+            print("???")
+            query = "select "+ columns + " from " + table + condition
+        else:
+            print("no such column")
+    elif table == "client":
+        colnames = ["client_id", "birth_number", "district_id", "bank_id"]
+        if columns in colnames:
+            if condition:
+                condition = " where " + condition
+            print("???")
+            query = "select "+ columns + " from " + table + condition
+        else:
+            print("no such column")
+    elif table == "disp":
+        colnames = ["disp_id", "client_id", "account_id", "type", "bank_id"]
+        if columns in colnames:
+            if condition:
+                condition = " where " + condition
+            print("???")
+            query = "select "+ columns + " from " + table + condition
+        else:
+            print("no such column")
+    elif table == "employees":
+        colnames = ["name", "age", "department", "bank_id"]
+        if columns in colnames:
+            if condition:
+                condition = " where " + condition
+            print("???")
+            query = "select "+ columns + " from " + table + condition
+        else:
+            print("no such column")
+    elif table == "loan":
+        colnames = ["loan_id", "account_id", "date", "amount", "duration", "payments", "status", "bank_id"]
+        if columns in colnames:
+            if condition:
+                condition = " where " + condition
+            print("???")
+            query = "select "+ columns + " from " + table + condition
+        else:
+            print("no such column")
+    elif table = "orders":
+        colnames = ["order_id", "account_id", "bank_to", "account_to", "amount", "k_symbol", "bank_id"]
+        if columns in colnames:
+            if condition:
+                condition = " where " + condition
+            print("???")
+            query = "select "+ columns + " from " + table + condition
+        else:
+            print("no such column")
+    elif table = "trans":
+        colnames = ["trans_id", "account_id", "date", "type", "operation", "amount", "balance", "k_symbol", "bank", "account", "bank_id"]
+        if columns in colnames:
+            if condition:
+                condition = " where " + condition
+            print("???")
+            query = "select "+ columns + " from " + table + condition
+        else:
+            print("no such column")
+
+    # if condition:
+    #     condition = " where " + condition
+    # print("???")
+    # query = "select "+ columns + " from " + table + condition
+    # print(username)
+    # print(query)
     body = {
         "query": query,
         "username": username
