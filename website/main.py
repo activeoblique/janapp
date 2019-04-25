02025de98a3aca3a826e6058ad27043e91414b3c
XY <oc2245@columbia.edu>
2019-04-25 00:44:51 -0400

    sdf

--- website/main.py
+++ website/main.py
@@ -8,7 +8,6 @@ import random
 import string
 from flask_wtf.csrf import CSRFProtect, CSRFError
 csrf = CSRFProtect(app)
-app.config['SERVER_NAME'] = 'localhost:5000'
 
 
 app.secret_key = b'19960223'
